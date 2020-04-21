295aa9847441282b6807da97493cfa9f416906d9
Sankhavaram Ram <saitulasiram94@gmail.com>
2020-04-21 14:32:04 +0200

    cors

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.get('/',function(req,res){
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');

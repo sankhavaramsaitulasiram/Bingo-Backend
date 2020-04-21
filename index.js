e6ad9d801d1c9bdfaf707a56ebbdc51412dee445
Sankhavaram Ram <saitulasiram94@gmail.com>
2020-04-21 14:24:43 +0200

    updating cors

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ app.get('/',function(req,res){
 })
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 const port = process.env.PORT ? process.env.PORT : 3000;
 app.listen(port, function () {
       console.log("Server started at port " + port);

7293e851bebd773dcd8b140bbe01891ac7ad2c80
Sankhavaram Ram <saitulasiram94@gmail.com>
2020-04-21 16:32:14 +0200

    Update cors config

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get('/',function(req,res){
-  console.log('Hey you are successful!!');
-})
-
-
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'https://apps-1692404577732836.apps.fbsbx.com/');
-  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
+
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+  console.log('Hey you are successful!!');
+})
 
 const port = process.env.PORT ? process.env.PORT : 3000;
 app.listen(port, function () {
